7c3de33a884d4340cb04c6b523d6dd93b83e2dc6
Stephen Domenici <sdomenici009@gmail.com>
2015-09-07 13:25:58 -0700

    State Machine, Battle Scene, Camera Rig

---
+++ Assets/_Scripts/Controller/InputController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class InputController : MonoBehaviour 
+{
+	private class Repeater
+	{
+		private const float threshold = 0.5f;
+		private const float rate = 0.25f;
+		private float _next;
+		private bool _hold;
+		private string _axis;
+		
+		public Repeater (string axisName)
+		{
+			_axis = axisName;
+		}
+		
+		public int Update ()
+		{
+			int retValue = 0;
+			int value = Mathf.RoundToInt( Input.GetAxisRaw(_axis) );
+			
+			if (value != 0)
+			{
+				if (Time.time > _next)
+				{
+					retValue = value;
+					_next = Time.time + (_hold ? rate : threshold);
+					_hold = true;
+				}
+			}
+			else
+			{
+				_hold = false;
+				_next = 0;
+			}
+			
+			return retValue;
+		}
+	}
+
+	private Repeater _hor = new Repeater("Horizontal");
+	private Repeater _ver = new Repeater("Vertical");
+
+	public static event EventHandler<InfoEventArgs<Point>> moveEvent;
+
+	void Update () 
+	{
+		int x = _hor.Update();
+		int y = _ver.Update();
+		if (x != 0 || y != 0)
+		{
+			if (moveEvent != null)
+				moveEvent(this, new InfoEventArgs<Point>(new Point(x, y)));
+		}
+
+		for (int i = 0; i < 3; ++i)
+		{
+			if (Input.GetButtonUp(_buttons[i]))
+			{
+				if (fireEvent != null)
+					fireEvent(this, new InfoEventArgs<int>(i));
+			}
+		}
+	}
+
+	public static event EventHandler<InfoEventArgs<int>> fireEvent;
+
+	private string[] _buttons = new string[] {"Fire1", "Fire2", "Fire3"};
+}
---
+++ Assets/_Scripts/State Machine/Battle States/MoveTargetState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTargetState : BattleState 
+{
+	protected override void OnMove (object sender, InfoEventArgs<Point> e)
+	{
+		SelectTile(e.info + pos);
+	}
+}
